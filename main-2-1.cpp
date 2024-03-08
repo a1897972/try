2ba19dad30785a6cadc19b5f565ecff388994c29
a1897972 <140569063+a1897972@users.noreply.github.com>
2024-03-08 21:20:17 +0800

    1
    1

---
+++ main-2-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+void print_binary_str(std::string decimal_number);
+
+int main() {
+    std::string decimal_number = "123456789";
+    print_binary_str(decimal_number);
+    
+    return 0;
+}
